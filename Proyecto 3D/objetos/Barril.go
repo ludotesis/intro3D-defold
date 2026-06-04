056ded8eaee28c7786cb6c8aca94349f51154eaf
Gaston Caminiti <gastoncaminiti@outlook.com>
2026-06-04 11:33:23 -0300

    Eliminar Objeto con ID

--- Proyecto 3D/objetos/Barril.go
+++ Proyecto 3D/objetos/Barril.go
@@ -5,8 +5,8 @@ components {
 embedded_components {
   id: "collisionobject"
   type: "collisionobject"
-  data: "type: COLLISION_OBJECT_TYPE_DYNAMIC\n"
-  "mass: 1.0\n"
+  data: "type: COLLISION_OBJECT_TYPE_TRIGGER\n"
+  "mass: 0.0\n"
   "friction: 0.1\n"
   "restitution: 0.5\n"
   "group: \"default\"\n"
